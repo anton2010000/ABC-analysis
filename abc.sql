d53e4296838c98902e65e775ed33c4d324b6e2df
anton2010000 <94874805+anton2010000@users.noreply.github.com>
2024-07-19 23:10:54 +0300

    Update for BI METABASE abc.sql

--- abc.sql
+++ abc.sql
@@ -1,25 +1,36 @@
-with product_sales as (select
-        dr_ndrugs as product,
-        sum(dr_kol) as amount,
-        sum(dr_kol*(dr_croz - dr_czak) - dr_sdisc) as profit,
-        sum(dr_kol*dr_croz - dr_sdisc) as revenue
-    from sales s
-    group by dr_ndrugs
+WITH product_sales AS (
+    SELECT
+        dr_ndrugs AS product,
+        SUM(dr_kol) AS amount,
+        SUM(dr_kol * (dr_croz - dr_czak) - dr_sdisc) AS profit,
+        SUM(dr_kol * dr_croz - dr_sdisc) AS revenue
+    FROM sales
+    GROUP BY dr_ndrugs
 )
-select product, 
-		case when sum(amount) over(order by amount desc) / sum(amount) over() <= 0.8 then 'A'
-	when sum(amount) over(order by amount desc) / sum(amount) over() <= 0.95 then 'B'
-	else 'C' 
-	end as amount_abc,	
-		case when sum(profit) over(order by profit desc) / sum(profit) over() <= 0.8 then 'A'
-	when sum(profit) over(order by profit desc) / sum(profit) over() <= 0.95 then 'B'
-	else 'C' 
-	end as profit_abc,	
-		case when sum(revenue) over(order by revenue desc) / sum(revenue) over() <= 0.8 then 'A'
-	when sum(revenue) over(order by revenue desc) / sum(revenue) over() <= 0.95 then 'B'
-	else 'C' 
-	end as revenue_abc
-from product_sales
-
-
-
+, main_query AS (
+    SELECT
+        product,
+        CASE 
+            WHEN SUM(amount) OVER(ORDER BY amount DESC) / SUM(amount) OVER() <= 0.8 THEN 'A'
+            WHEN SUM(amount) OVER(ORDER BY amount DESC) / SUM(amount) OVER() <= 0.95 THEN 'B'
+            ELSE 'C'
+        END AS amount_abc,
+        CASE 
+            WHEN SUM(profit) OVER(ORDER BY profit DESC) / SUM(profit) OVER() <= 0.8 THEN 'A'
+            WHEN SUM(profit) OVER(ORDER BY profit DESC) / SUM(profit) OVER() <= 0.95 THEN 'B'
+            ELSE 'C'
+        END AS profit_abc,
+        CASE 
+            WHEN SUM(revenue) OVER(ORDER BY revenue DESC) / SUM(revenue) OVER() <= 0.8 THEN 'A'
+            WHEN SUM(revenue) OVER(ORDER BY revenue DESC) / SUM(revenue) OVER() <= 0.95 THEN 'B'
+            ELSE 'C'
+        END AS revenue_abc
+    FROM product_sales
+)
+SELECT *
+FROM main_query
+WHERE 1=1
+  [[AND product = {{product}}]]
+  [[AND amount_abc = {{amount_abc}}]]
+  [[AND profit_abc = {{profit_abc}}]]
+  [[AND revenue_abc = {{revenue_abc}}]]
